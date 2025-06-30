dcdf4246ac75cf60c25295d15259ef1867c42d76
David Dikenshtein <dikenshtein@bmpgmbh.de>
2025-06-30 11:46:16 +0200

    New Main Controller

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\MainController;
 
-Route::get('/', function () {
-    return view('home');
-});
+
+Route::get('/', [MainController::class, 'home']) -> name('home');
